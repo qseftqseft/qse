bd1703916ef8889c297c356aa0323ce61231910f
qseft <ulrichv@seznam.cz>
2025-07-27 15:19:49 +0200

    proof-of-concept

---
+++ qse/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace qse
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch sw  = new Stopwatch();
+            
+            
+            sw.Start();
+            
+            int left = Console.WindowWidth-1;
+            int top = Console.WindowHeight-1;
+            int num = 0;
+            string filename = "/home/qseft/test";
+            string originalfile = File.ReadAllText(filename).Replace("\t", "    ");
+            string filestr = "";
+            List<char> file = new List<char>();
+            foreach (char c in originalfile)
+            {
+                file.Add(c);
+            }
+            
+
+            List<int> filelenghts = new List<int>();
+
+            
+            
+            Console.CursorVisible = false;
+            
+            Console.Clear();
+            Console.CursorVisible = true;
+            int line = 1;
+            int column = 0;
+            int scroll = 0;
+            
+            while (true)
+            {
+                num = 0;
+                filelenghts.Add(0);
+                filestr = string.Concat(file);
+                filelenghts = lenghts(filestr);
+                
+                write(scroll, top, left,filelenghts, file, filename, filestr, line, column);
+                
+                Console.SetCursorPosition(column, line);
+                
+                ConsoleKeyInfo keyInfo1 = Console.ReadKey(true);
+                switch (keyInfo1.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        line--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        line++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        column--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        column++;
+                        break;
+                    case ConsoleKey.Backspace:
+                        file.RemoveAt(filelenghts[line-1]+column-2+line);
+                        column--;
+                        break;
+                    case ConsoleKey.Enter:
+                        file.Insert(filelenghts[line-1]+column+line,  '\n');
+                        column=0;
+                        line++;
+                        break;
+                    case ConsoleKey.Delete:
+                        file.RemoveAt(filelenghts[line-1]+column+line-1);
+                        break;
+                    case ConsoleKey.PageUp:
+                        scroll--;
+                        break;
+                    case ConsoleKey.PageDown:
+                        scroll++;
+                        break;
+                    
+                    default:
+                        file.Insert(filelenghts[line-1]+column-1+line,  keyInfo1.KeyChar);
+                        column++;
+                        break;
+                }
+
+                if (line <= 0)
+                    line = 1;
+                if (line >= top)
+                    line = top - 1;
+                
+                if (column < 0)
+                    column = 0;
+                if (column > left)
+                    column = left ;
+                
+                
+            }
+        }
+
+        public static List<int> lenghts(string file)
+        {
+            int num = 0;
+            List<int> filelenghts = new List<int>();
+            filelenghts.Add(0);
+            foreach (string lne in file.Split('\n'))
+            {
+                num = num + lne.Length;
+                filelenghts.Add(num);
+            }
+            return filelenghts;
+        }
+
+        public static void write(int scroll, int top, int left, List<int> filelenghts, List<char> file, string filename, string filestr,int line, int column)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(0, 0);
+            Console.Write(" QSE");
+            for (int i = 0; i < left-(4+filename.Length); i++)
+                Console.Write(" ");
+            Console.Write(filename+" ");
+            Console.ResetColor();
+            
+            Console.SetCursorPosition(0, 1);
+            
+            for (int i = scroll; i < filelenghts[scroll+top+1]-filelenghts[scroll+1]; i++)
+                Console.Write(file[i]);
+            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(0, top);
+            Console.Write(" " + filelenghts.Count.ToString() + " lines loaded");
+            for (int i = 0; i < left-(14+filelenghts.Count.ToString().Count()+(filelenghts[line-1]+column-1+line).ToString().Count()); i++)
+                Console.Write(" ");
+            Console.Write(filelenghts[line - 1] + column - 1 + line + " ");
+            Console.ResetColor();
+        }
+
+    }
+}
